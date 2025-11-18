71f375d3d184d90fd745ae2a4c2a6012dab0e673
archit24singh <architsingh439@gmail.com>
2025-11-18 13:26:41 -0600

    6th commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,11 @@
 import Hero from '../components/Hero';
 import Showcase from '../components/Showcase';
 import Footer from '../components/Footer';
+import Menu from '../components/Menu';
 
 const Home = () => {
   return (
-    <main>
+    <main className="bg-gray-900 min-h-screen w-full flex flex-col items-center pt-32">
       <Hero />
       <Showcase />
       <Footer />
